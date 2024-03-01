95e1928fb4b95b71e33757d773fd8c80f7dc4637
kailahartman <kailahartman123@gmail.com>
2024-03-01 11:39:54 +0200

    consolidate duplicate code: for general item sellIn zero or less

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -16,24 +16,38 @@ final class GildedRose
 
     public function updateQuality(): void
     {
-        foreach ($this->items as $item) {
+        foreach ($this->items as $item) 
+        {
             if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
+                if ($item->quality > 0) 
+                {
+                    if ($item->name !== 'Sulfuras, Hand of Ragnaros') 
+                    {
+                        if ($item->sellIn <= 0)
+                        {
+                            $item->quality = $item->quality - 1;
+                        }
                         $item->quality = $item->quality - 1;
                     }
                 }
-            } else {
-                if ($item->quality < 50) {
+            } 
+            else {
+                if ($item->quality < 50) 
+                {
                     $item->quality = $item->quality + 1;
-                    if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
+                    if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') 
+                    {
+                        if ($item->sellIn < 11) 
+                        {
+                            if ($item->quality < 50) 
+                            {
                                 $item->quality = $item->quality + 1;
                             }
                         }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
+                        if ($item->sellIn < 6) 
+                        {
+                            if ($item->quality < 50) 
+                            {
                                 $item->quality = $item->quality + 1;
                             }
                         }
@@ -41,23 +55,24 @@ final class GildedRose
                 }
             }
 
-            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name !== 'Sulfuras, Hand of Ragnaros') 
+            {
                 $item->sellIn = $item->sellIn - 1;
             }
 
-            if ($item->sellIn < 0) {
-                if ($item->name !== 'Aged Brie') {
-                    if ($item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
+            if ($item->sellIn < 0) 
+            {
+                if ($item->name !== 'Aged Brie') 
+                {
+                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert')
+                    {
                         $item->quality = $item->quality - $item->quality;
                     }
-                } else {
-                    if ($item->quality < 50) {
+                } 
+                else 
+                {
+                    if ($item->quality < 50) 
+                    {
                         $item->quality = $item->quality + 1;
                     }
                 }
