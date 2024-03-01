58407f47a3e950804a9ffdca7643edfe7843862a
kailahartman <kailahartman123@gmail.com>
2024-03-01 11:57:40 +0200

    changing the sulfuras statement to positive and restructured nested condition for readability

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -18,19 +18,20 @@ final class GildedRose
     {
         foreach ($this->items as $item) 
         {
-            if ($item->name !== 'Sulfuras, Hand of Ragnaros')
+            if ($item->name == 'Sulfuras, Hand of Ragnaros')
+            {
+
+            }
+            else
             {
                 if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
                     if ($item->quality > 0) 
                     {
-                        
-                        {
                             if ($item->sellIn <= 0)
                             {
                                 $item->quality = $item->quality - 1;
                             }
                             $item->quality = $item->quality - 1;
-                        }
                     }
                 } 
                 else {
@@ -39,16 +40,13 @@ final class GildedRose
                         $item->quality = $item->quality + 1;
                         if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') 
                         {
-                            if ($item->sellIn < 11) 
+                            if ($item->quality < 50) 
                             {
-                                if ($item->quality < 50) 
+                                if ($item->sellIn < 11) 
                                 {
-                                    $item->quality = $item->quality + 1;
+                                    $item->quality = $item->quality + 1;  
                                 }
-                            }
-                            if ($item->sellIn < 6) 
-                            {
-                                if ($item->quality < 50) 
+                                if ($item->sellIn < 6) 
                                 {
                                     $item->quality = $item->quality + 1;
                                 }
@@ -56,11 +54,7 @@ final class GildedRose
                         }
                     }
                 }
-
-                
                 $item->sellIn = $item->sellIn - 1;
-            
-
                 if ($item->sellIn < 0) 
                 {
                     if ($item->name == 'Aged Brie') 
