9c213568d125a227daa03160030c9f0cb2239faa
kailahartman <kailahartman123@gmail.com>
2024-03-01 12:20:39 +0200

    improve readability

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -24,17 +24,19 @@ final class GildedRose
             }
             else
             {
-                if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') 
+                {
                     if ($item->quality > 0) 
                     {
-                            if ($item->sellIn <= 0)
-                            {
-                                $item->quality = $item->quality - 1;
-                            }
+                        if ($item->sellIn <= 0)
+                        {
                             $item->quality = $item->quality - 1;
+                        }
+                        $item->quality = $item->quality - 1;
                     }
                 } 
-                else {
+                else 
+                {
                     if ($item->quality < 50) 
                     {
                         $item->quality = $item->quality + 1;
@@ -65,7 +67,7 @@ final class GildedRose
                     {
                         if ($item->name == 'Backstage passes to a TAFKAL80ETC concert')
                         {
-                            $item->quality = $item->quality - $item->quality;
+                            $item->quality = 0;
                         }
                         
                     }
