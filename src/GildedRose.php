ce1185cba7589aca206ff702a05c8b8a30044aec
kailahartman <kailahartman123@gmail.com>
2024-03-01 12:59:29 +0200

    place the switch case in a seperate function to improve readability

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -19,7 +19,12 @@ final class GildedRose
         foreach ($this->items as $item) 
         {
             $item->sellIn = $item->sellIn - 1;
-            switch ($item->name) 
+            $this->updateItem($item);      
+        }
+    }
+    public function updateItem(Item $item): void
+    {
+        switch ($item->name) 
             {
                 case ('Sulfuras, Hand of Ragnaros'):
                 {
@@ -42,59 +47,55 @@ final class GildedRose
                     $this->updateDefault($item);
                 }
             }
-                
-                
+    }
+    public function updateAgedBrie(Item $item): void
+    {
+        if ($item->sellIn < 0 and $item->quality < 50)
+        {
+            $item->quality = $item->quality + 1;
+        }
+        if ($item->quality < 50) 
+        {
+            $item->quality = $item->quality + 1;
+        }
+    }
+
+    public function updateBackstage(Item $item): void
+    {
+        if ($item->quality < 50) 
+        {
+            $item->quality = $item->quality + 1;
+            if ($item->sellIn < 10) 
+            {
+                $item->quality = $item->quality + 1;  
+            }
+            if ($item->sellIn < 5) 
+            {
+                $item->quality = $item->quality + 1;
             }
         }
-
-        public function updateAgedBrie(Item $item): void
+        if ($item->sellIn < 0)
         {
-            if ($item->sellIn < 0 and $item->quality < 50)
-            {
-                $item->quality = $item->quality + 1;
-            }
-            if ($item->quality < 50) 
-            {
-                $item->quality = $item->quality + 1;
-            }
+            $item->quality = 0;
         }
+    }
 
-        public function updateBackstage(Item $item): void
-        {
-            if ($item->quality < 50) 
-            {
-                $item->quality = $item->quality + 1;
-                if ($item->sellIn < 10) 
+    public function updateSulfuras(Item $item): void
+    {
+        $item->sellIn = $item->sellIn + 1;
+    }
+
+    public function updateDefault(Item $item): void
+    {
+        if ($item->quality > 0) 
                 {
-                    $item->quality = $item->quality + 1;  
-                }
-                if ($item->sellIn < 5) 
-                {
-                    $item->quality = $item->quality + 1;
-                }
-            }
-            if ($item->sellIn < 0)
-            {
-                $item->quality = 0;
-            }
-        }
-
-        public function updateSulfuras(Item $item): void
-        {
-            $item->sellIn = $item->sellIn + 1;
-        }
-        
-        public function updateDefault(Item $item): void
-        {
-            if ($item->quality > 0) 
+                    if ($item->sellIn < 0)
                     {
-                        if ($item->sellIn < 0)
-                        {
-                            $item->quality = $item->quality - 1;
-                        }
                         $item->quality = $item->quality - 1;
                     }
-        }
+                    $item->quality = $item->quality - 1;
+                }
+    }
 
     }
 
