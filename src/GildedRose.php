8848b3de2531565fb337dc4e29861e64c957b306
kailahartman <kailahartman123@gmail.com>
2024-03-01 11:48:08 +0200

    remove all if Sulfuras statements and creating one general one

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -18,66 +18,69 @@ final class GildedRose
     {
         foreach ($this->items as $item) 
         {
-            if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) 
-                {
-                    if ($item->name !== 'Sulfuras, Hand of Ragnaros') 
+            if ($item->name !== 'Sulfuras, Hand of Ragnaros')
+            {
+                if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->quality > 0) 
                     {
-                        if ($item->sellIn <= 0)
+                        
                         {
+                            if ($item->sellIn <= 0)
+                            {
+                                $item->quality = $item->quality - 1;
+                            }
                             $item->quality = $item->quality - 1;
                         }
-                        $item->quality = $item->quality - 1;
                     }
-                }
-            } 
-            else {
-                if ($item->quality < 50) 
-                {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') 
-                    {
-                        if ($item->sellIn < 11) 
-                        {
-                            if ($item->quality < 50) 
-                            {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) 
-                        {
-                            if ($item->quality < 50) 
-                            {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($item->name !== 'Sulfuras, Hand of Ragnaros') 
-            {
-                $item->sellIn = $item->sellIn - 1;
-            }
-
-            if ($item->sellIn < 0) 
-            {
-                if ($item->name == 'Aged Brie') 
-                {
+                } 
+                else {
                     if ($item->quality < 50) 
                     {
                         $item->quality = $item->quality + 1;
+                        if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') 
+                        {
+                            if ($item->sellIn < 11) 
+                            {
+                                if ($item->quality < 50) 
+                                {
+                                    $item->quality = $item->quality + 1;
+                                }
+                            }
+                            if ($item->sellIn < 6) 
+                            {
+                                if ($item->quality < 50) 
+                                {
+                                    $item->quality = $item->quality + 1;
+                                }
+                            }
+                        }
                     }
-                } 
-                else 
+                }
+
+                
+                $item->sellIn = $item->sellIn - 1;
+            
+
+                if ($item->sellIn < 0) 
                 {
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert')
+                    if ($item->name == 'Aged Brie') 
                     {
-                        $item->quality = $item->quality - $item->quality;
+                        if ($item->quality < 50) 
+                        {
+                            $item->quality = $item->quality + 1;
+                        }
+                    } 
+                    else 
+                    {
+                        if ($item->name == 'Backstage passes to a TAFKAL80ETC concert')
+                        {
+                            $item->quality = $item->quality - $item->quality;
+                        }
+                        
                     }
-                    
                 }
             }
         }
+    
     }
 }
